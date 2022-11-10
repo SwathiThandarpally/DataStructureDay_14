7846380f17f2862810443dcd658b6f4b9c0ad067
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-10 13:56:38 +0530

    Added program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace DataStructure
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Display();
+
+            Queue queue = new Queue();
+            queue.Display();
+
+
+
+
+           
+
+
+
+        }
+    }
+}
+
