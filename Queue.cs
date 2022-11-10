018b7b7a5d19eec172cb52167f58c07e3f209ca2
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-10 14:02:12 +0530

    Added usecase 3 and usecase4 program

---
+++ Queue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Queue
+    {
+        public void Display()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(56);
+            queue.Enqueue(36);
+            queue.Enqueue(70);
+            foreach(var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            queue.Dequeue();
+            Console.WriteLine("After dequeue");
+            foreach(int item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
